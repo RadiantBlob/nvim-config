a9d42c38679953d5b5dac6e100cc5bdb93fde5ee
RadiantBlob <mullerfelix178@gmail.com>
2025-05-01 12:09:44 +0200

    replaced some plugins with mini.nvim alternatives

--- init.lua
+++ init.lua
@@ -11,6 +11,20 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local path_package = vim.fn.stdpath('data') .. '/site'
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    -- Uncomment next line to use 'stable' branch
+    -- '--branch', 'stable',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+end
+
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (copying) text',
 	group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
@@ -24,3 +38,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('settings')
 require('remap')
 require('lazy').setup('plugins')
+
+require('mini.comment').setup()
+require('mini.surround').setup()
+require('mini.align').setup()
+require('mini.ai').setup()
